70ff74ed9fd7067222c752efebe9d2e4351c7887
shizk233 <wangwangdaxian233@gmail.com>
2020-08-15 23:25:41 +0800

    thread local

---
+++ src/main/java/com/shizk/demo/java/core/concrurrent/threadlocal/ThreadLocalDemo2.java
@@ -0,0 +1,17 @@
+package com.shizk.demo.java.core.concrurrent.threadlocal;
+
+public class ThreadLocalDemo2 {
+    private static final ThreadLocal<Integer> TL_INT    = ThreadLocal.withInitial(() -> 6);
+    private static final ThreadLocal<String>  TL_STRING = ThreadLocal.withInitial(() -> "Hello, world");
+
+    public static void main(String... args) {
+        // 6
+        System.out.println(TL_INT.get());
+        TL_INT.set(TL_INT.get() + 1);
+        // 7
+        System.out.println(TL_INT.get());
+        TL_INT.remove();
+        // 会重新初始化该value，6
+        System.out.println(TL_INT.get());
+    }
+}
